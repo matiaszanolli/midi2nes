808a9b68c49f62ded78f9fe4cfdeeedf5ad33d47
matiaszanolli <mzanolli@sophilabs.com>
2016-12-06 15:20:20 -0300

    Converts a single track into NES ASM properly

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 var neslySound = require('nesly-sound');
 
 var midiFileParser = require('midi-file-parser');
-var fs = require('fs')
+var fs = require('fs');
+var _ = require('lodash');
+var tonal = require('tonal');
 
 
 // file = window.atob(file)
@@ -11,16 +13,30 @@ class Generator {
     this.track = track;
     this.generator = context[name];
     this.timing = timing;
+    this.processed = [];
+  }
+
+  convertNote(note) {
+    console.log('Converting ', note)
+    return tonal.note.fromMidi(note.noteNumber);
+  }
+
+  notes(note) {
+    return note.subtype === 'noteOn';
   }
 
   generate() {
-    this.generator(['C5', 'E5', 'G5', 'C6']).timing(this.timing);
+    this.processed = _.map(this.track.filter(this.notes), (note) => {
+      return this.convertNote(note);
+    });
+    console.log(this.processed)
+    this.generator(this.processed).timing(this.timing);
   }
 }
 
 class Song {
   constructor(file) {
-    var f = fs.readFileSync(file, 'binary')
+    const f = fs.readFileSync(file, 'binary');
 
     this.context = neslySound(); // the Nesly engine
     this.timing = 1/8; // the default timing
@@ -29,19 +45,26 @@ class Song {
     this.tracks = {} // midi tracks
   }
 
+  // Gets a midi unprocessed track from its id
   getMidiTrack(id) {
+    console.log('Getting MIDI track: ', id)
     return this.midi.tracks[id];
   }
 
+  // Sets an unprocessed given track
   setTrack(name, track) {
-    track = new Generator(track, this.context, name, this.timing)
+    console.log('Setting MIDI track: ', name)
+    this.tracks[name] = new Generator(track, this.context, name, this.timing)
   }
 
+  // Outputs the NES ASM source code
   write() {
+    console.log('Writing...')
     this.context.done();
     this.context.write();
   }
 
+  // Processes all tracks on the engine
   generateTracks() {
     for(var track in this.tracks) {
       this.tracks[track].generate();
@@ -50,9 +73,9 @@ class Song {
 }
 
 song = new Song('Adventure_Island_Level_1.mid');
-song.setTrack('square1', song.getMidiTrack(0));
-console.log(song.tracks.square1);
+song.setTrack('square1', song.getMidiTrack(1));
 song.generateTracks();
+// fs.writeFile('./build/tracks.json', JSON.stringify(song.midi.tracks, null, 2) , 'utf-8');
 song.write();
 
 //song.square1(['C5', 'E5', 'G5', 'C6'])
